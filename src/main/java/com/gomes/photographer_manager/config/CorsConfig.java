582c0cd5b4a61df9d3acc1a9d1cc078d8a3dff64
brunaogomes11 <brunogomesper@gmail.com>
2026-06-18 13:18:50 -0300

    fix: mais uma tentativa de corrigir cors

--- src/main/java/com/gomes/photographer_manager/config/CorsConfig.java
+++ src/main/java/com/gomes/photographer_manager/config/CorsConfig.java
@@ -1,14 +1,36 @@
 package com.gomes.photographer_manager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOriginPatterns(List.of("*"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(List.of("*"));
+		config.setExposedHeaders(List.of("Content-Disposition"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOriginPatterns("*")
+		registry.addMapping("/**")
+				.allowedOriginPatterns("*")
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
 				.allowCredentials(true)
 				.allowedHeaders("*")
